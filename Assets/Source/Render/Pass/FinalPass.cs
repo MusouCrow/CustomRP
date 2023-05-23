fb95972672f0a27ddf86301da7327aec7d0566f2
MusouCrow <939549924@qq.com>
2023-05-23 13:38:12 +0800

    Make profiling scope

--- Assets/Source/Render/Pass/FinalPass.cs
+++ Assets/Source/Render/Pass/FinalPass.cs
@@ -4,13 +4,22 @@ using UnityEngine.Experimental.Rendering;
 
 namespace Game.Render {
     public class FinalPass : IRenderPass {
+        private ProfilingSampler profilingSampler;
+
+        public FinalPass() {
+            this.profilingSampler = new ProfilingSampler("FinalPass");
+        }
+
         public void Render(ref ScriptableRenderContext context, ref RenderData data) {
-            var id = Shader.PropertyToID("_TestTexture");
-            var srcRTI = new RenderTargetIdentifier(id);
-            var dstRTI = new RenderTargetIdentifier(data.camera.targetTexture);
-            
             var cmd = CommandBufferPool.Get();
-            cmd.Blit(srcRTI, dstRTI);
+
+            using (new ProfilingScope(cmd, this.profilingSampler)) {
+                var id = Shader.PropertyToID("_TestTexture");
+                var srcRTI = new RenderTargetIdentifier(id);
+                var dstRTI = new RenderTargetIdentifier(data.camera.targetTexture);
+                cmd.Blit(srcRTI, dstRTI);
+            }
+            
             context.ExecuteCommandBuffer(cmd);
             CommandBufferPool.Release(cmd);
         }
--- Assets/Source/Render/Pass/TestPass.cs
+++ Assets/Source/Render/Pass/TestPass.cs
@@ -4,24 +4,34 @@ using UnityEngine.Experimental.Rendering;
 
 namespace Game.Render {
     public class TestPass : IRenderPass {
+        private ProfilingSampler profilingSampler;
+
+        public TestPass() {
+            this.profilingSampler = new ProfilingSampler("TestPass");
+        }
+
         public void Render(ref ScriptableRenderContext context, ref RenderData data) {
-            var rtd = data.cameraRTD;
-            var id = Shader.PropertyToID("_TestTexture");
-            var rti = new RenderTargetIdentifier(id);
-
             var cmd = CommandBufferPool.Get();
-            cmd.GetTemporaryRT(id, rtd, FilterMode.Bilinear);
-            cmd.SetRenderTarget(rti, 
-                RenderBufferLoadAction.Load, RenderBufferStoreAction.Store,
-                RenderBufferLoadAction.Load, RenderBufferStoreAction.DontCare
-            );
-            cmd.ClearRenderTarget(true, true, Color.black);
-            context.ExecuteCommandBuffer(cmd);
-            CommandBufferPool.Release(cmd);
 
-            var drawingSettings = RenderUtil.CreateDrawingSettings(ref data, "Forward");
-            var filteringSettings = FilteringSettings.defaultValue;
-            context.DrawRenderers(data.cullingResults, ref drawingSettings, ref filteringSettings);
+            using (new ProfilingScope(cmd, this.profilingSampler)) {
+                var rtd = data.cameraRTD;
+                var id = Shader.PropertyToID("_TestTexture");
+                var rti = new RenderTargetIdentifier(id);
+
+                cmd.GetTemporaryRT(id, rtd, FilterMode.Bilinear);
+                cmd.SetRenderTarget(rti, 
+                    RenderBufferLoadAction.Load, RenderBufferStoreAction.Store,
+                    RenderBufferLoadAction.Load, RenderBufferStoreAction.DontCare
+                );
+                cmd.ClearRenderTarget(true, true, Color.black);
+
+                context.ExecuteCommandBuffer(cmd);
+                CommandBufferPool.Release(cmd);
+
+                var drawingSettings = RenderUtil.CreateDrawingSettings(ref data, "Forward");
+                var filteringSettings = FilteringSettings.defaultValue;
+                context.DrawRenderers(data.cullingResults, ref drawingSettings, ref filteringSettings);
+            }
         }
 
         public void Clean(ref ScriptableRenderContext context, ref RenderData data) {
