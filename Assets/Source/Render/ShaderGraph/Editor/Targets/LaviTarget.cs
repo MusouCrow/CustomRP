f14f6cd391364f7455ca6b4ca8507f071adce561
MusouCrow <939549924@qq.com>
2023-05-31 13:35:24 +0800

    Making shader graph support

---
+++ Assets/Source/Render/ShaderGraph/Editor/Targets/LaviTarget.cs
@@ -0,0 +1,19 @@
+using UnityEditor.ShaderGraph;
+
+namespace Game.Render.ShaderGraph {
+    /*
+    class LaviTarget : Target {
+        public LaviTarget() {
+
+        }
+
+        public override bool IsActive() {
+            return false;
+        }
+        
+        public override void Setup(ref TargetSetupContext context) {
+            
+        }
+    }
+    */
+}
